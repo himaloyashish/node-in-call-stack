f6812621a3e2b2820aa4c6aaa1b336daba15ffca
himaloyash <himaloyashis98@gmail.com>
2024-04-28 08:58:54 +0600

    os is checked

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const _= require("loadsh");
+const _= require("lodash");
+const os = require("os");
+
 const people = require("./people");
 
-console.log(people);
+// console.log(_.last(people));
+
+console.log(os.platform());
