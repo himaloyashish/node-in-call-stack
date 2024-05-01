9410692928d79b8bd5079db369bb1a47c06409e0
himaloyash <himaloyashis98@gmail.com>
2024-05-01 14:29:48 +0600

    bigData is ok

--- streamBuffer/index.js
+++ streamBuffer/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const ourReadStream  = fs.createReadStream(`${__dirname}/bigData.txt`, "utf8");
+const ourReadStream  = fs.createReadStream(`${__dirname}/bigData.txt`, "utf-8");
 
 ourReadStream.on("data", (data)=>{
 
