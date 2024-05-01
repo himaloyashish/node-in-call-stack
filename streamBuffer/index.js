26d895aebdf12212c3db5e2b191553f18d4f2e93
himaloyash <himaloyashis98@gmail.com>
2024-05-01 14:28:42 +0600

    utf - 8 ok

--- streamBuffer/index.js
+++ streamBuffer/index.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 
-const ourReadStream  = fs.createReadStream(`${__dirname}/bigData.txt`);
+const ourReadStream  = fs.createReadStream(`${__dirname}/bigData.txt`, "utf8");
 
-ourReadStream.on("data", (chunk)=>{
+ourReadStream.on("data", (data)=>{
 
-    console.log(chunk);
+    console.log(data);
 
 })
