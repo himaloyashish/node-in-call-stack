07c8261b84dd033fd045bc82af57d879844d1757
himaloyash <himaloyashis98@gmail.com>
2024-05-02 13:34:25 +0600

    all the write stream is done and chunk also

--- streamBuffer/writeStream.js
+++ streamBuffer/writeStream.js
@@ -2,9 +2,10 @@ const http = require("http");
 const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-   
+    const myReadStream = fs.createReadStream(`${__dirname}/bigData.txt`, 'utf8');
+
+    myReadStream.pipe(res);
 })
 
-
-server.listen(3000);
-console.log("listening on port 3000");
+server.listen(3000)
+console.log('The server is run on 3000');
