6ad09656365aae0fb5fa5b9168f581fcfbe510b4
himaloyash <himaloyashis98@gmail.com>
2024-05-04 03:24:05 +0600

    all is ok about node application

---
+++ lesson7/math.js
@@ -0,0 +1,18 @@
+/**
+ * Title : Math Library
+ * Description : Utility Library for math related functions
+ * Author: Himaloy Bala Ashish
+ * Date : 3 / 4 / 2024
+*/
+
+// math object - module scaffolding
+
+const math = {};
+
+// Get a random tow integer between two integer
+
+math.getRandomNumber = function getRandomNumber (min, max) {
+
+    
+
+}
