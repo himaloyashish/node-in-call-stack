e0e94b08b2660d50c256bcf9572d4b63e434dcf4
himaloyash <himaloyashis98@gmail.com>
2024-05-04 12:21:42 +0600

    all the quotes are done into array

--- lesson7/lib/quotes/index.js
+++ lesson7/lib/quotes/index.js
@@ -7,3 +7,27 @@
 */
 
 
+// Dependency
+const fs = require('fs');
+
+// Quotes object - module scaffolding
+const quotes = {};
+
+
+// Get all the quotes
+
+quotes.allQuote = function allQuote () {
+
+    // Read the text file containing the quotes
+    const fileContents = fs.readFileSync(`${__dirname}/quotes.txt`, 'utf8');
+    
+    // The string into array
+    const arrayOfQuotes = fileContents.split(/\r?\n/);
+
+    // Return the array.
+    return arrayOfQuotes ;
+
+}
+
+module.exports = quotes;
+
