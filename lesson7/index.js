bc96625891ca2fee9539c14111a14f8986820d7a
himaloyash <himaloyashis98@gmail.com>
2024-05-04 13:01:22 +0600

    the node applicatiion done

--- lesson7/index.js
+++ lesson7/index.js
@@ -8,8 +8,9 @@
 
 // Dependency
 
-const mathLibrary = require("./lib");
-const QuoteLibrary = require("./lib");
+const mathLibrary = require("./lib/math");
+const quoteLibrary = require("./lib/quotes");
+
 
 // App object - Module scaffolding
 const app = {};
@@ -21,11 +22,23 @@ app.config = {
 
 app.printAQuote = function printAQuote() {
   // get all the quotes
-  const allQuotes = "";
+  const allQuotes = quoteLibrary.allQuote() ;
+
+  // Get the length of the quote library.
+  const numberOfQuotes = allQuotes.length;
+
+  // Pick a random number between 6 and 1 
+  const randomQuotes = mathLibrary.getRandomNumber(1, numberOfQuotes);
+  
+  // Get that quote at that position in the array ( One Minus )
+  const selectQuote = allQuotes[randomQuotes - 1];
+
+  console.log(selectQuote);
+
 };
 
 app.indefiniteLoop = function indefiniteLoop() {
-    setInterval('', 1);
+    setInterval( app.printAQuote , app.config.timeBetweenQuote );
 };
 
-// app.indefiniteLoop();
+app.indefiniteLoop();
